b6199bd8875ec5427db2ef3473b06b743586295e
Sudhir B <bsudhir2001@gmail.com>
2020-08-08 14:29:33 +0530

    Second Commit

--- Add.java
+++ Add.java
@@ -5,7 +5,7 @@ public class Add {
         Scanner input = new Scanner(System.in);
         int num1 = input.nextInt();
         int num2 = input.nextInt();
-        int result = num1 + num2;
+        int result = num1 - num2;
         System.out.pritnln("The Addition of " + (num1) + " and " + (num2) + "is: " + (result));
     }
 }
